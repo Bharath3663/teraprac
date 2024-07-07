5d83513a70cb87360d4f5b6162617b4359544bdb
bharath vallabaneni <154310464+Bharath3663@users.noreply.github.com>
2024-07-07 10:31:40 +0530

    Create ebs_volume.tf

---
+++ terraform/ebs_volume.tf
@@ -0,0 +1,6 @@
+resource "aws_ebs_volume" "ebs_volume" {
+  availability_zone = var.availability_zone
+  size               = var.volume_size
+  type               = var.volume_type
+  tags               = var.tags
+}
