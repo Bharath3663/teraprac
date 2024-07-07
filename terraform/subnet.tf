db9bd7d5f10b34beae5bd83416d6a393f1eac5b2
bharath vallabaneni <154310464+Bharath3663@users.noreply.github.com>
2024-07-07 20:13:02 +0530

    Update subnet.tf

--- terraform/subnet.tf
+++ terraform/subnet.tf
@@ -1,9 +1,20 @@
-resource "aws_subnet" "public_subnet" {
-  vpc_id     = aws_vpc.my_vpc.id
-  cidr_block = var.public_subnet_cidr
+resource "aws_subnet" "Public-1" {
+  vpc_id = aws_vpc.NEW.id
+  cidr_block = var.public_subnet_cidr_block
   availability_zone = var.availability_zone
 
   tags = {
-    Name = "Public Subnet"
+    Name = "Public-1"
   }
 }
+
+resource "aws_subnet" "Private-1" {
+  vpc_id = aws_vpc.NEW.id
+  cidr_block = var.private_subnet_cidr_block
+  availability_zone = var.availability_zone
+
+  tags = {
+    Name = "Private-1"
+  }
+}
+
