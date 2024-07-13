f886c76175ff15cb0c935dc7b31e8f81d4d55cf0
bharath vallabaneni <154310464+Bharath3663@users.noreply.github.com>
2024-07-13 14:50:55 +0530

    Update routetable.tf

--- terraform/routetable.tf
+++ terraform/routetable.tf
@@ -1,1 +1,16 @@
+resource "aws_route_table" "root" {
+  vpc_id = aws_vpc.NEW.id
+  tags = {
+    Name = var.route_table_name
+  }
+}
 
+resource "aws_route_table_association" "public_subnet_association" {
+  subnet_id = aws_subnet.Public-1.id
+  route_table_id = aws_route_table.root.id
+}
+
+resource "aws_route_table_association" "internet_gateway_association" {
+  gateway_id = aws_internet_gateway.gw.id
+  route_table_id = aws_route_table.root.id
+}
